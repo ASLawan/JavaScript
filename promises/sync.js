bf0944f4be1869362b4e802f68a34d48ba980b04
ASLawan <nawalnitsua@gmail.com>
2024-06-07 19:54:05 +0100

    Synchrouse execution

---
+++ promises/sync.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+//Synchronouse
+
+console.log("I\n");
+console.log("am\n");
+console.log("learning\n");
+console.log("to\n");
+console.log("code\n");
+console.log("with\n");
+console.log("JavaScript");
