ef766431983fb987ec8dda03b0e1ba1ae5cc75bf
ASLawan <nawalnitsua@gmail.com>
2024-06-07 19:54:27 +0100

    Asynchronouse execution

---
+++ promises/async.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+//Synchronouse
+
+console.log("I\n");
+console.log("am\n");
+setTimeout(() => {
+	console.log("learning\n");
+}, 5000);
+console.log("to\n");
+console.log("code\n");
+console.log("with\n");
+console.log("JavaScript\n");
